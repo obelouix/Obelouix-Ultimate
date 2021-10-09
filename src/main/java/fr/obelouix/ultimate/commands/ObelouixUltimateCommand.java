6d349b6a27e54da7573b43b90d575a724069eacb
Louis <3874662+obelouix@users.noreply.github.com>
2021-10-09 22:59:46 +0200

    completed version argument

--- src/main/java/fr/obelouix/ultimate/commands/ObelouixUltimateCommand.java
+++ src/main/java/fr/obelouix/ultimate/commands/ObelouixUltimateCommand.java
@@ -1,7 +1,12 @@
 package fr.obelouix.ultimate.commands;
 
 import com.google.common.collect.ImmutableList;
+import fr.obelouix.ultimate.ObelouixUltimate;
+import fr.obelouix.ultimate.i18n.I18n;
 import fr.obelouix.ultimate.permissions.IPermission;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +17,8 @@ import java.util.List;
 
 public class ObelouixUltimateCommand extends BukkitCommand {
 
+    private static final ObelouixUltimate plugin = ObelouixUltimate.getInstance();
+    private static final I18n i18n = I18n.getInstance();
     private static final List<String> subcommands = ImmutableList.of("reload", "version");
 
     public ObelouixUltimateCommand(String commandName) {
@@ -21,7 +28,7 @@ public class ObelouixUltimateCommand extends BukkitCommand {
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, String alias, String[] args) {
         final List<String> subcommandList = new ArrayList<>(Collections.emptyList());
-        for (String subcommand : subcommands){
+        for (final String subcommand : subcommands){
             if(sender.hasPermission("obelouix.command.obelouixultimate." + subcommand)){
                 subcommandList.add(subcommand);
             }
@@ -34,7 +41,12 @@ public class ObelouixUltimateCommand extends BukkitCommand {
         if(IPermission.hasPermission(commandSender, "obelouix.command.obelouixultimate")){
             if(args.length == 1){
                 if(args[0].equalsIgnoreCase("version") && IPermission.hasPermission(commandSender, "obelouix.command.obelouixultimate.version")){
-
+                    Component message = Component.text(i18n.getTranslation(commandSender, "obelouix.plugin.version")).color(NamedTextColor.GREEN)
+                            .replaceText(TextReplacementConfig.builder()
+                                    .matchLiteral("{0}")
+                                    .replacement(Component.text(plugin.getDescription().getVersion()).color(NamedTextColor.AQUA))
+                                    .build());
+                    commandSender.sendMessage(message);
                 }
             }
         }
