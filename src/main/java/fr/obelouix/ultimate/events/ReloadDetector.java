a4698a82ab73973fd37fd285bd0b5b27e4529450
Louis <3874662+obelouix@users.noreply.github.com>
2021-10-10 13:00:09 +0200

    optimized event

--- src/main/java/fr/obelouix/ultimate/events/ReloadDetector.java
+++ src/main/java/fr/obelouix/ultimate/events/ReloadDetector.java
@@ -1,5 +1,6 @@
 package fr.obelouix.ultimate.events;
 
+import com.google.common.collect.ImmutableList;
 import fr.obelouix.ultimate.i18n.I18n;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
@@ -12,16 +13,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import java.util.List;
+
 public class ReloadDetector implements Listener {
 
     private static final I18n i18n = I18n.getInstance();
+    private final List<String> commands = ImmutableList.of("/reload", "/reload confirm", "/rl", "/rl confirm");
 
     @EventHandler
     public void commandPreProcess(PlayerCommandPreprocessEvent event){
         final Player player = event.getPlayer();
-        if(event.getMessage().equalsIgnoreCase("/reload") || event.getMessage().equalsIgnoreCase("/reload confirm") ||
-                event.getMessage().equalsIgnoreCase("/rl") || event.getMessage().equalsIgnoreCase("/rl confirm")) {
-            if(player.hasPermission("bukkit.command.reload")) {
+        for(String match: commands){
+            if(event.getMessage().equalsIgnoreCase(match) && player.hasPermission("bukkit.command.reload")){
                 final Component message = Component.text(i18n.getTranslation(player, "obelouix.reload_cmd_is_bad"))
                         .color(NamedTextColor.DARK_RED)
                         .replaceText(TextReplacementConfig.builder()
@@ -31,6 +34,8 @@ public class ReloadDetector implements Listener {
                                         .clickEvent(ClickEvent.openUrl("https://madelinemiller.dev/blog/problem-with-reload/")))
                                 .build());
                 player.sendMessage(message);
+
+                break;
             }
         }
     }
