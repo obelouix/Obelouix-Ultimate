a80ac021ee5d258714e36d47e57811c991c819f0
Louis <3874662+obelouix@users.noreply.github.com>
2021-10-11 19:14:12 +0200

    added more comments in the config

--- src/main/java/fr/obelouix/ultimate/config/Config.java
+++ src/main/java/fr/obelouix/ultimate/config/Config.java
@@ -65,7 +65,19 @@ public class Config {
                 for (Group group : LuckPermsUtils.getGroups()) {
                     root.node("chat").act(n -> {
                         if (group.getName().equals("default")) {
-                            n.node("format").node(group.getName()).set("&#808080{displayname}: {message}")
+                            n.node("format")
+                                    .commentIfAbsent("""
+                                            This allow you to control the chat formatting for every groups
+                                            You case use standard color code like &4 and also hex color codes like &#808080
+                                                                                        
+                                            Some tags are available to allow you to fully customize the chat:
+                                            - {displayname} : player name
+                                            - {message} : the message of the player
+                                            - {world} : the current world of the player
+                                            - {prefix} : the luckperms prefix of the group
+                                            - {suffix} : the luckperms suffix of the group
+                                            """)
+                                    .node(group.getName()).set("&#808080{displayname}: {message}")
                                     .commentIfAbsent("default LuckPerms group");
                         } else {
                             n.node("format").node(group.getName()).set("&#32cd32[{world}]&r{prefix}{displayname}{suffix}: &r{message}")
