6fed4760ed503a57c18fd2c5129f4e740fe3ef3d
Louis <3874662+obelouix@users.noreply.github.com>
2021-10-12 12:53:53 +0200

    finished day command

--- src/main/java/fr/obelouix/ultimate/commands/DayCommand.java
+++ src/main/java/fr/obelouix/ultimate/commands/DayCommand.java
@@ -3,8 +3,10 @@ package fr.obelouix.ultimate.commands;
 import fr.obelouix.ultimate.i18n.I18n;
 import fr.obelouix.ultimate.messages.PluginMessages;
 import fr.obelouix.ultimate.permissions.IPermission;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
@@ -16,6 +18,8 @@ import java.util.Objects;
 public class DayCommand extends BukkitCommand {
 
     private static final I18n i18n = I18n.getInstance();
+    private static Component message;
+    private static CommandSender sender;
 
     public DayCommand(String name) {
         super(name);
@@ -24,23 +28,22 @@ public class DayCommand extends BukkitCommand {
 
     @Override
     public boolean execute(@NotNull CommandSender commandSender, @NotNull String label, @NotNull String[] args) {
+        sender = commandSender;
         if (commandSender instanceof Player player && IPermission.hasPermission(commandSender, "obelouix.command.day")) {
             if (args.length == 0) {
-                setDay(player.getWorld());
+                setDay(player.getWorld().getName());
             } else if (args.length == 1) {
-                if (Bukkit.getWorld(args[0]) != null) {
-                    setDay(Objects.requireNonNull(Bukkit.getWorld(args[0])));
-                }
+                setDay(args[0]);
             } else {
                 player.sendMessage(PluginMessages.wrongCommandUsage(this, player));
             }
+            player.sendMessage(message);
         } else {
             if (args.length == 0) {
                 commandSender.sendMessage(i18n.getTranslation(commandSender, "obelouix.command.day.console.too_few_arguments"));
             } else if (args.length == 1) {
-                if (Bukkit.getWorld(args[0]) != null) {
-                    setDay(Objects.requireNonNull(Bukkit.getWorld(args[0])));
-                }
+                setDay(args[0]);
+                commandSender.sendMessage(message);
             } else {
                 commandSender.sendMessage(i18n.getTranslation(commandSender, "obelouix.command.day.console.too_many_arguments"));
             }
@@ -53,8 +56,23 @@ public class DayCommand extends BukkitCommand {
         return super.tabComplete(sender, alias, args);
     }
 
-    private void setDay(World world) {
-        Objects.requireNonNull(Bukkit.getWorld(world.getName())).setTime(0);
+    private void setDay(String world) {
+        if (Bukkit.getWorld(world) != null) {
+            Objects.requireNonNull(Bukkit.getWorld(world)).setTime(0);
+            if (sender instanceof Player player) {
+                message = PluginMessages.playerTimeMessage(player, 0);
+            } else {
+                message = PluginMessages.playerTimeMessage(sender, world, 0);
+            }
+
+        } else {
+            message = Component.text(i18n.getTranslation(sender, "obelouix.command.day.world_non_existent"))
+                    .color(NamedTextColor.DARK_RED)
+                    .replaceText(TextReplacementConfig.builder()
+                            .matchLiteral("{0}")
+                            .replacement(Component.text(world).color(NamedTextColor.RED))
+                            .build());
+        }
     }
 
 }
--- src/main/java/fr/obelouix/ultimate/messages/PluginMessages.java
+++ src/main/java/fr/obelouix/ultimate/messages/PluginMessages.java
@@ -6,18 +6,95 @@ import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.DecimalFormat;
 
 public class PluginMessages {
 
     private static final I18n i18n = I18n.getInstance();
+    private static final DecimalFormat format = new DecimalFormat("00");
+    private static long worldHour;
+    private static long worldMinute;
 
-    public static Component wrongCommandUsage(Command command, CommandSender commandSender){
+    /**
+     * Create a component that will give the correct use of a given command
+     *
+     * @param command       - the command that will be displayed in the message
+     * @param commandSender - the player/console who sended the command
+     * @return a formatted message with the correct use of the given command
+     */
+    public static Component wrongCommandUsage(Command command, CommandSender commandSender) {
         return Component.text(i18n.getTranslation(commandSender, "obelouix.wrong_command_usage"))
                 .color(NamedTextColor.DARK_RED)
                 .replaceText(TextReplacementConfig.builder()
                         .matchLiteral("{0}")
                         .replacement(Component.text(command.getUsage()).color(NamedTextColor.RED))
                         .build());
+    }
+
+    /**
+     * Send a message returning the time of the world based on the player world
+     *
+     * @param sender - the player
+     * @param time   - time of the world in ticks
+     * @return a formatted component with world's time
+     */
+    public static Component playerTimeMessage(CommandSender sender, int time) {
+        final Player player = (Player) sender;
+        calculateTime(time);
+
+        return Component.text(i18n.getTranslation(player, "obelouix.command.time.set"))
+                .color(NamedTextColor.GOLD)
+                .replaceText(TextReplacementConfig.builder()
+                        .matchLiteral("{0}")
+                        .replacement(Component.text(player.getWorld().getName())
+                                .color(NamedTextColor.RED))
+                        .build())
+                .replaceText(TextReplacementConfig.builder()
+                        .matchLiteral("{1}")
+                        .replacement(Component.text(format.format(worldHour) + "h" + format.format(worldMinute))
+                                .color(NamedTextColor.RED))
+                        .build());
+    }
+
+    /**
+     * Send a message returning the time of the world based on the world name given in argument
+     *
+     * @param sender - console
+     * @param world  - the world name
+     * @param time   - time of the world in ticks
+     * @return a formatted component with world's time
+     */
+    public static Component playerTimeMessage(CommandSender sender, String world, int time) {
+        calculateTime(time);
+
+        return Component.text(i18n.getTranslation(sender, "obelouix.command.time.set"))
+                .color(NamedTextColor.GOLD)
+                .replaceText(TextReplacementConfig.builder()
+                        .matchLiteral("{0}")
+                        .replacement(Component.text(world)
+                                .color(NamedTextColor.RED))
+                        .build())
+                .replaceText(TextReplacementConfig.builder()
+                        .matchLiteral("{1}")
+                        .replacement(Component.text(format.format(worldHour) + "h" + format.format(worldMinute))
+                                .color(NamedTextColor.RED))
+                        .build());
+    }
+
+    /**
+     * Allows to calculate real time of the world in human format
+     *
+     * @param time - time of the world in ticks
+     */
+    private static void calculateTime(int time) {
+        worldHour = time / 1000 + 6;
+        worldMinute = (time % 1000) * 60 / 1000;
+
+        if (worldHour > 23) {
+            worldHour = worldHour - 24;
+        }
     }
 
 }
