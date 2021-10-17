031d92d6da5bde287ada3e57bf96e6498b957773
Louis <3874662+obelouix@users.noreply.github.com>
2021-10-17 16:43:44 +0200

    added more api

--- src/main/java/fr/obelouix/ultimate/api/InventoryAPI.java
+++ src/main/java/fr/obelouix/ultimate/api/InventoryAPI.java
@@ -7,10 +7,13 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.List;
 import java.util.UUID;
 
 public class InventoryAPI {
@@ -62,6 +65,60 @@ public class InventoryAPI {
         skullMeta.displayName(title.decoration(TextDecoration.ITALIC, false));
         skull.setItemMeta(skullMeta); // Apply the modified meta to the initial created item
         return skull;
+    }
+
+    /**
+     * allows to add an {@link Enchantment}
+     *
+     * @param item         the item that will receive the enchantment
+     * @param enchantment  the {@link Enchantment} to add
+     * @param enchantLevel an {@link Integer} between {@code 0} and {@link 255}
+     */
+    public static void addEnchantment(ItemStack item, Enchantment enchantment, int enchantLevel) {
+        final ItemMeta meta = item.getItemMeta();
+        meta.setUnbreakable(true);
+        meta.addEnchant(enchantment, enchantLevel, true);
+        item.setItemMeta(meta);
+    }
+
+    /**
+     * allows to add multiple {@link Enchantment} at once
+     *
+     * @param item         the item that will receive the enchantments
+     * @param enchantments a {@link List} of {@link Enchantment}
+     * @param enchantLevel an {@link Integer} between {@code 0} and {@link 255}
+     */
+    public static void addEnchantments(ItemStack item, List<Enchantment> enchantments, int enchantLevel) {
+        final ItemMeta meta = item.getItemMeta();
+        meta.setUnbreakable(true);
+        for (final Enchantment enchantment : enchantments) {
+            meta.addEnchant(enchantment, enchantLevel, true);
+        }
+        item.setItemMeta(meta);
+    }
+
+    /**
+     * change the name of the item
+     *
+     * @param item     the item were we change the name
+     * @param itemName a {@link Component} that will be the name if this item
+     */
+    public static void setItemName(ItemStack item, Component itemName) {
+        final ItemMeta meta = item.getItemMeta();
+        meta.displayName(itemName.decoration(TextDecoration.ITALIC, false));
+        item.setItemMeta(meta);
+    }
+
+    /**
+     * add lore (aka description) to the item
+     *
+     * @param item       the item were whe add the lore
+     * @param components a {@link List} of {@link Component} to show
+     */
+    public static void addLore(ItemStack item, List<Component> components) {
+        final ItemMeta meta = item.getItemMeta();
+        meta.lore(components);
+        item.setItemMeta(meta);
     }
 
 }
