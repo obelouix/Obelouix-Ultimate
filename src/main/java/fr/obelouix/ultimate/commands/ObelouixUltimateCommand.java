e2cab1d0bc656fb2b8920065c55519028adc3c77
Louis <3874662+obelouix@users.noreply.github.com>
2021-12-24 19:44:54 +0100

    command rewritten with cloud framework

--- src/main/java/fr/obelouix/ultimate/commands/ObelouixUltimateCommand.java
+++ src/main/java/fr/obelouix/ultimate/commands/ObelouixUltimateCommand.java
@@ -3,95 +3,64 @@ package fr.obelouix.ultimate.commands;
 import cloud.commandframework.ArgumentDescription;
 import cloud.commandframework.arguments.standard.StringArgument;
 import cloud.commandframework.context.CommandContext;
-import cloud.commandframework.meta.CommandMeta;
+import cloud.commandframework.execution.preprocessor.CommandPreprocessingContext;
 import com.google.common.collect.ImmutableList;
 import fr.obelouix.ultimate.ObelouixUltimate;
+import fr.obelouix.ultimate.audience.MessageSender;
 import fr.obelouix.ultimate.commands.manager.CommandManager;
+import fr.obelouix.ultimate.config.Config;
 import fr.obelouix.ultimate.i18n.I18n;
 import fr.obelouix.ultimate.permissions.IPermission;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.List;
 
-public class ObelouixUltimateCommand /*extends BukkitCommand*/ {
+public class ObelouixUltimateCommand {
 
     private static final ObelouixUltimate plugin = ObelouixUltimate.getInstance();
     private static final I18n i18n = I18n.getInstance();
     private static final List<String> subcommands = ImmutableList.of("reload", "version");
 
-/*    public ObelouixUltimateCommand(String commandName) {
-        super(commandName);
-        this.setUsage("/obelouixultimate <reload | version>");
-    }*/
-
-    public void register() throws Exception {
-        @NonNull CommandMeta meta = CommandMeta.simple().build();
+    public void register() {
         CommandManager.getInstance().command(
                 CommandManager.getInstance().commandBuilder("obelouixultimate")
-                        .argument(StringArgument.optional("world"), ArgumentDescription.of("world"))
+                        .argument(StringArgument.single("version/reload"), ArgumentDescription.of("return the version of the plugin"))
                         .handler(this::execute)
                         .build()
-        );
+        ).setCommandSuggestionProcessor(this::suggestions);
+    }
+
+    private List<String> suggestions(@NonNull CommandPreprocessingContext<CommandSender> commandSenderCommandPreprocessingContext, @NonNull List<String> strings) {
+        return subcommands;
     }
 
     private void execute(@NonNull CommandContext<CommandSender> context) {
         final CommandSender sender = context.getSender();
-
+        final String argument = context.get("version/reload");
         if (IPermission.hasPermission(sender, "obelouix.command.obelouixultimate")) {
-            Component message;
-            final String world;
-
-            if (context.getOptional("world").isPresent()) {
-                world = String.valueOf(context.getOptional("world").get());
-                sender.sendMessage(world);
-            }
-        }
-    }
-
-   /* @Override
-    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, String[] args) {
-        final List<String> subcommandList = new ArrayList<>(Collections.emptyList());
-        for (final String subcommand : subcommands) {
-            if (sender.hasPermission("obelouix.command.obelouixultimate." + subcommand)) {
-                subcommandList.add(subcommand);
-            }
-        }
-        return subcommandList;
-    }
-
-    @Override
-    public boolean execute(@NotNull CommandSender commandSender, @NotNull String commandLabel, @NotNull String[] args) {
-        if (IPermission.hasPermission(commandSender, "obelouix.command.obelouixultimate")) {
-            Component message;
-            if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("version") && IPermission.hasPermission(commandSender, "obelouix.command.obelouixultimate.version")) {
-                    message = Component.text(i18n.getTranslation(commandSender, "obelouix.plugin.version")).color(NamedTextColor.GREEN)
-                            .replaceText(TextReplacementConfig.builder()
-                                    .matchLiteral("{0}")
-                                    .replacement(Component.text(plugin.getDescription().getVersion()).color(NamedTextColor.AQUA))
-                                    .build());
-
-                } else if (args[0].equalsIgnoreCase("reload")) {
-                    Config.loadConfig();
-                    if(Config.isConfigReloaded()) {
-                        message = Component.text(i18n.getTranslation(commandSender, "obelouix.command.obelouixultimate.reload"))
-                                .color(NamedTextColor.AQUA);
-                    } else {
-                        message = Component.text(i18n.getTranslation(commandSender, "obelouix.command.obelouixultimate.reload.failed"))
-                                .color(NamedTextColor.DARK_RED);
-                    }
-
+            if (argument.equalsIgnoreCase("version")) {
+                MessageSender.sendMessage(sender,
+                        Component.text(I18n.getInstance().getTranslation(sender, "obelouix.plugin.version"))
+                                .color(NamedTextColor.GREEN)
+                                .replaceText(TextReplacementConfig.builder()
+                                        .matchLiteral("{0}")
+                                        .replacement(Component.text(plugin.getDescription().getVersion()).color(NamedTextColor.AQUA))
+                                        .build()
+                                ));
+            } else if (argument.equalsIgnoreCase("reload")) {
+                Config.loadConfig();
+                if (Config.isConfigReloaded()) {
+                    MessageSender.sendMessage(sender, Component.text(i18n.getTranslation(sender, "obelouix.command.obelouixultimate.reload"))
+                            .color(NamedTextColor.AQUA));
                 } else {
-                    message = PluginMessages.wrongCommandUsage(this, commandSender);
+                    MessageSender.sendMessage(sender, Component.text(i18n.getTranslation(sender, "obelouix.command.obelouixultimate.reload.failed"))
+                            .color(NamedTextColor.DARK_RED));
                 }
-            } else {
-                message = PluginMessages.wrongCommandUsage(this, commandSender);
             }
-            commandSender.sendMessage(message);
         }
-        return false;
-    }*/
-
+    }
 }
