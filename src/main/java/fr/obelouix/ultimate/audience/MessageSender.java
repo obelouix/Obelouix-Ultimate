1b31153f516efd3fd162ebb82ad14445c554b3f2
Louis <3874662+obelouix@users.noreply.github.com>
2021-12-24 17:36:49 +0100

    added broadcast methods

--- src/main/java/fr/obelouix/ultimate/audience/MessageSender.java
+++ src/main/java/fr/obelouix/ultimate/audience/MessageSender.java
@@ -4,14 +4,47 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
 
 public class MessageSender {
 
+    /**
+     * Send a message to a command sender
+     *
+     * @param sender  the command sender
+     * @param message a component
+     */
     public static void sendMessage(CommandSender sender, Component message) {
+        Audience audience = Audience.audience(Objects.requireNonNull(Bukkit.getPlayer(sender.getName())));
+        audience.sendMessage(message);
+    }
 
-        Audience audience = Audience.audience((Audience) Objects.requireNonNull(Bukkit.getPlayer(sender.getName())));
+    /**
+     * Send a message to every connected players
+     *
+     * @param message a component
+     */
+    public static void broadcast(Component message) {
+        Audience audience = Audience.audience(Bukkit.getOnlinePlayers());
+        audience.sendMessage(message);
+    }
+
+    /**
+     * Send a message to every connected players that as the required permission
+     *
+     * @param permission the permission the player need to receive the message
+     * @param message    a component
+     */
+    public static void broadcast(String permission, Component message) {
+        Collection<Player> authorizedPlayers = new java.util.ArrayList<>(Collections.emptyList());
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission(permission)) authorizedPlayers.add(player);
+        }
+        Audience audience = Audience.audience(authorizedPlayers);
         audience.sendMessage(message);
     }
 
