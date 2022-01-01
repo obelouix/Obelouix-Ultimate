e588113ef1c9285f2adde0e85df34f0a68b5d4c0
Louis <3874662+obelouix@users.noreply.github.com>
2022-01-01 15:19:01 +0100

    improved coordinates bar

--- src/main/java/fr/obelouix/ultimate/coordinates/Coordinates.java
+++ src/main/java/fr/obelouix/ultimate/coordinates/Coordinates.java
@@ -24,31 +24,35 @@ public class Coordinates implements Listener {
 
     private static final ObelouixUltimate plugin = ObelouixUltimate.getInstance();
 
-    public @NotNull BukkitTask runTask(Player player) {
+    public @NotNull BukkitTask runTask() {
         return new BukkitRunnable() {
 
             @Override
             public void run() {
-                final HoconConfigurationLoader playerFile = HoconConfigurationLoader.builder()
-                        .path(Path.of(plugin.getDataFolder().getPath(), "data", "players", player.getName() + ".conf"))
-                        .build();
-                try {
-                    final CommentedConfigurationNode root = playerFile.load();
-                } catch (ConfigurateException e) {
-                    e.printStackTrace();
-                }
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    final HoconConfigurationLoader playerFile = HoconConfigurationLoader.builder()
+                            .path(Path.of(plugin.getDataFolder().getPath(), "data", "players", player.getName() + ".conf"))
+                            .build();
+                    final CommentedConfigurationNode root;
+                    try {
+                        root = playerFile.load();
+                        if (!root.node("language").empty()) {
 
-                Component actionBar = Component.text("X: ", NamedTextColor.DARK_RED)
-                        .append(Component.text(player.getLocation().getBlockX(), NamedTextColor.WHITE))
-                        .append(Component.text(" Y: ", NamedTextColor.GREEN))
-                        .append(Component.text(player.getLocation().getBlockY(), NamedTextColor.WHITE))
-                        .append(Component.text(" Z: ", NamedTextColor.DARK_BLUE))
-                        .append(Component.text(player.getLocation().getBlockZ(), NamedTextColor.WHITE))
-                        .append(Component.text(" " + parse24(player.getWorld().getTime()) + " ", NamedTextColor.GOLD))
-                        .append(Component.text(" Direction: ", NamedTextColor.AQUA))
-                        .append(Component.text(getFacing(player), NamedTextColor.WHITE));
-                MessageSender.sendActionBar(player.getPlayer(), actionBar);
-
+                            Component actionBar = Component.text("X: ", NamedTextColor.DARK_RED)
+                                    .append(Component.text(player.getLocation().getBlockX(), NamedTextColor.WHITE))
+                                    .append(Component.text(" Y: ", NamedTextColor.GREEN))
+                                    .append(Component.text(player.getLocation().getBlockY(), NamedTextColor.WHITE))
+                                    .append(Component.text(" Z: ", NamedTextColor.DARK_BLUE))
+                                    .append(Component.text(player.getLocation().getBlockZ(), NamedTextColor.WHITE))
+                                    .append(Component.text(" " + parseTo24(player.getWorld().getTime()) + " ", NamedTextColor.GOLD))
+                                    .append(Component.text(" Direction: ", NamedTextColor.AQUA))
+                                    .append(Component.text(getFacing(player), NamedTextColor.WHITE));
+                            MessageSender.sendActionBar(player.getPlayer(), actionBar);
+                        }
+                    } catch (ConfigurateException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         }.runTaskTimerAsynchronously(plugin, 0, 5);
     }
@@ -56,44 +60,18 @@ public class Coordinates implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
         if (!Bukkit.getOnlinePlayers().isEmpty()) {
-            if (!Bukkit.getScheduler().isCurrentlyRunning(runTask(playerJoinEvent.getPlayer()).getTaskId())) {
-                runTask(playerJoinEvent.getPlayer());
+            if (!Bukkit.getScheduler().isCurrentlyRunning(runTask().getTaskId())) {
+                runTask();
             }
         } else {
-            if (Bukkit.getScheduler().isCurrentlyRunning(runTask(playerJoinEvent.getPlayer()).getTaskId())) {
+            if (Bukkit.getScheduler().isCurrentlyRunning(runTask().getTaskId())) {
                 // Cancel the task if it's running and there is no player connected
-                runTask(playerJoinEvent.getPlayer()).cancel();
+                runTask().cancel();
             }
         }
     }
 
-  /*  @EventHandler
-    public void onPlayerMove(PlayerMoveEvent playerMoveEvent) {
-        Player player = playerMoveEvent.getPlayer();
-        if (!playerMoveEvent.hasChangedPosition() || playerMoveEvent.hasChangedPosition()) {
-            final HoconConfigurationLoader playerFile = HoconConfigurationLoader.builder()
-                    .path(Path.of(plugin.getDataFolder().getPath(), "data", "players", player.getName() + ".conf"))
-                    .build();
-            try {
-                final CommentedConfigurationNode root = playerFile.load();
-            } catch (ConfigurateException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Component actionBar = Component.text("X: ", NamedTextColor.DARK_RED)
-                .append(Component.text(player.getLocation().getBlockX(), NamedTextColor.WHITE))
-                .append(Component.text(" Y: ", NamedTextColor.GREEN))
-                .append(Component.text(player.getLocation().getBlockY(), NamedTextColor.WHITE))
-                .append(Component.text(" Z: ", NamedTextColor.DARK_BLUE))
-                .append(Component.text(player.getLocation().getBlockZ(), NamedTextColor.WHITE))
-                .append(Component.text(" " + parse24(player.getWorld().getTime()) + " ", NamedTextColor.GOLD))
-                .append(Component.text(" Direction: ", NamedTextColor.AQUA))
-                .append(Component.text(getFacing(player), NamedTextColor.WHITE));
-        MessageSender.sendActionBar(playerMoveEvent.getPlayer(), actionBar);
-    }*/
-
-    private String parse24(final long time) {
+    private @NotNull String parseTo24(final long time) {
         long hours = time / 1000L + 6L;
         final long minutes = time % 1000L * 60L / 1000L;
         if (hours == 24L) {
@@ -117,38 +95,37 @@ public class Coordinates implements Listener {
         if (hours == 30L) {
             hours = 6L;
         }
-        String minutes_ = "0" + minutes;
-        return hours + ":" + minutes_.substring(minutes_.length() - 2);
+        return hours + ":" + ("0" + minutes).substring(("0" + minutes).length() - 2);
     }
 
     private String getFacing(final Player p) {
         final double yaw = p.getLocation().getYaw();
 
-        if (yaw >= 337.5 || (yaw <= 22.5 && yaw >= 0.0) || (yaw >= -22.5 && yaw <= 0.0) || (yaw <= -337.5 && yaw <= 0.0)) {
+        if (yaw >= 337.5 || yaw <= 22.5 && yaw >= 0.0 || yaw >= -22.5 && yaw <= 0.0 || yaw <= -337.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.south").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 22.5 && yaw <= 67.5) || (yaw <= -292.5 && yaw >= -337.5)) {
+        if (yaw >= 22.5 && yaw <= 67.5 || yaw <= -292.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.southwest").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 67.5 && yaw <= 112.5) || (yaw <= -247.5 && yaw >= -292.5)) {
+        if (yaw >= 67.5 && yaw <= 112.5 || yaw <= -247.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.west").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 112.5 && yaw <= 157.5) || (yaw <= -202.5 && yaw >= -247.5)) {
+        if (yaw >= 112.5 && yaw <= 157.5 || yaw <= -202.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.northwest").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 157.5 && yaw <= 202.5) || (yaw <= -157.5 && yaw >= -202.5)) {
+        if (yaw >= 157.5 && yaw <= 202.5 || yaw <= -157.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.north").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 202.5 && yaw <= 247.5) || (yaw <= -112.5 && yaw >= -157.5)) {
+        if (yaw >= 202.5 && yaw <= 247.5 || yaw <= -112.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.northeast").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 247.5 && yaw <= 292.5) || (yaw <= -67.5 && yaw >= -112.5)) {
+        if (yaw >= 247.5 && yaw <= 292.5 || yaw <= -67.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.east").toUpperCase(Locale.ROOT);
         }
-        if ((yaw >= 292.5 && yaw <= 337.5) || (yaw <= -22.5 && yaw >= -67.5)) {
+        if (yaw >= 292.5 || yaw <= -22.5) {
             return I18n.getInstance().getTranslation(p, "obelouix.direction.southeast").toUpperCase(Locale.ROOT);
         }
-        return "Error!";
+        return "error";
     }
 
 }
