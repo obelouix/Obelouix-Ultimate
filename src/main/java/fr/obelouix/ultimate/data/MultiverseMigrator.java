b11179d2f30bae44fe8056eb28f54817c8b52e9b
Louis <3874662+obelouix@users.noreply.github.com>
2022-02-07 00:30:38 +0100

    started to import multiverse worlds

--- src/main/java/fr/obelouix/ultimate/data/MultiverseMigrator.java
+++ src/main/java/fr/obelouix/ultimate/data/MultiverseMigrator.java
@@ -2,15 +2,19 @@ package fr.obelouix.ultimate.data;
 
 import fr.obelouix.ultimate.ObelouixUltimate;
 import org.bukkit.Bukkit;
+import org.checkerframework.checker.units.qual.K;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+import org.spongepowered.configurate.serialize.SerializationException;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.NavigableMap;
+import java.util.Objects;
 import java.util.TreeMap;
 
 public class MultiverseMigrator {
@@ -43,14 +47,34 @@ public class MultiverseMigrator {
                 final CommentedConfigurationNode mvRoot = multiverseLoader.load();
 
                 NavigableMap<Object, CommentedConfigurationNode> mvMap = new TreeMap<>(mvRoot.node("worlds").childrenMap());
-
-                for (Object map: mvMap.entrySet()){
-                    plugin.getLogger().info(String.valueOf(map));
-                    if (map.equals("==: MVWorld")){
-                        plugin.getLogger().info(String.valueOf(mvMap.lowerEntry(map)));
+                mvMap.forEach((o, commentedConfigurationNode) ->
+                {
+                    // Is PVP enabled in this world ?
+                    final boolean isPVPEnabled = mvRoot.node("worlds", o.toString(), "pvp").getBoolean();
+                    // Get the seed of the world
+                    final String seed = mvRoot.node("worlds", o.toString(), "seed").getString();
+                    // Get the world type (Normal, Nether, The_End)
+                    final String environment = mvRoot.node("worlds", o.toString(), "environment").getString();
+                    // Get the game mode of the world
+                    final String gamemode = mvRoot.node("worlds", o.toString(), "gameMode").getString();
+                    // Should we keep the spawn of this world in ram ?
+                    final boolean keepSpawnInMemory = mvRoot.node("worlds", o.toString(), "keepSpawnInMemory").getBoolean();
+                    // Should we load the world when the server start ?
+                    final boolean loadWorldAtStartup = mvRoot.node("worlds", o.toString(), "autoLoad").getBoolean();
+                    try {
+                        root.node("worlds", o.toString(), "pvp").set(isPVPEnabled);
+                        root.node("worlds", o.toString(), "seed").set(seed);
+                        root.node("worlds", o.toString(), "keepSpawnInMemory").set(keepSpawnInMemory);
+                        root.node("worlds", o.toString(), "environment").set(environment);
+                        root.node("worlds", o.toString(), "gamemode").set(gamemode);
+                        root.node("worlds", o.toString(), "autoLoad").set(loadWorldAtStartup)
+                                .commentIfAbsent("Should the server load the world at startup ?");
+                    } catch (SerializationException e) {
+                        e.printStackTrace();
                     }
+                });
 
-                }
+                worldLoader.save(root);
 
                 /*mvRoot.node("worlds").act(value -> {
                     mvRoot.node("worlds").childrenMap().forEach((o, commentedConfigurationNode) ->
