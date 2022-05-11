64e10f6351eb80f0e4146562c1895a0325afb0ba
Louis <3874662+obelouix@users.noreply.github.com>
2022-05-11 13:44:20 +0200

    enhanced update checker

--- src/main/java/fr/obelouix/ultimate/utils/Updater.java
+++ src/main/java/fr/obelouix/ultimate/utils/Updater.java
@@ -12,14 +12,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 
 public class Updater {
 
     private static final ObelouixUltimate plugin = ObelouixUltimate.getInstance();
-    private static JsonParser parser = null;
 
     public Updater() {
+        // 216000 -> 3 hours
         checkForUpdate().runTaskTimerAsynchronously(plugin, 0, 216000);
     }
 
@@ -38,18 +37,33 @@ public class Updater {
                     plugin.getLogger().info("Checking for updates...");
                     result = JsonParser.parseReader(reader).getAsJsonObject().getAsJsonPrimitive("tag_name");
 
-                    if (!Objects.equals(result.getAsString(), plugin.getDescription().getVersion())) {
-                        plugin.getLogger().warning("You are using a DEV build");
-                        plugin.getLogger().info("Update available, download it here: https://github.com/obelouix/Obelouix-Ultimate/releases/latest");
+                    if (updateAvailable(plugin.getDescription().getVersion().replace("-SNAPSHOT", ""), result.getAsString().replace("-SNAPSHOT", ""))) {
+                        boolean isBothSnapshot = plugin.getDescription().getVersion().contains("-SNAPSHOT") && result.getAsString().contains("-SNAPSHOT");
+                        if (isBothSnapshot) {
+                            plugin.getLogger().info("A new snapshot is available, download it here: https://github.com/obelouix/Obelouix-Ultimate/releases/latest");
+                        } else {
+                            if (!isBothSnapshot) {
+                                plugin.getLogger().info("A new update available, download it here: https://github.com/obelouix/Obelouix-Ultimate/releases/latest");
+                            } else plugin.getLogger().info("We are up-to-date!");
+                        }
+
                     } else plugin.getLogger().info("We are up-to-date!");
+
 
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
-            // 216000 -> 3 hours
         };
     }
 
+    private boolean updateAvailable(String pluginVersion, String githubPluginVersion) {
+        String[] pVersion = pluginVersion.split("\\.");
+        String[] gVersion = githubPluginVersion.split("\\.");
+        for (int i = 0; i < 3; i++) {
+            if (Integer.parseInt(gVersion[i]) > Integer.parseInt(pVersion[i])) return true;
+        }
+        return false;
+    }
 
 }
