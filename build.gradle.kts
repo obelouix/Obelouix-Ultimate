ce1c780e811abe4350a5e875cbf76e4a81554f75
Louis <3874662+obelouix@users.noreply.github.com>
2022-05-12 15:59:29 +0200

    updated plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "fr.obelouix.ultimate"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.1-SNAPSHOT"
 
 java {
     // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
