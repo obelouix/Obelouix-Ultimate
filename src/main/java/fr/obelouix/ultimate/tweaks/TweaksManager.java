d1d6b2a0c2ed061bed4277590255bc6e10825170
Louis <3874662+obelouix@users.noreply.github.com>
2022-06-01 14:44:22 +0200

    cleanup

--- src/main/java/fr/obelouix/ultimate/tweaks/TweaksManager.java
+++ src/main/java/fr/obelouix/ultimate/tweaks/TweaksManager.java
@@ -9,7 +9,6 @@ public class TweaksManager extends Events {
     public TweaksManager() {
 
         final double expMergeRadius = SpigotConfig.config.getDouble("world-settings.default.merge-radius.exp");
-        System.out.println(expMergeRadius);
         if (expMergeRadius > 0. && Config.giveEnderDragonExp()) addEvent(new EnderDragonTweaks());
 
         registerEvents();
