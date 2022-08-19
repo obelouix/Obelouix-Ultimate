97d96ad09136ddaacfe2f58df7f791132424edac
Louis <3874662+obelouix@users.noreply.github.com>
2022-08-19 22:45:18 +0200

    added javadoc

--- src/main/java/fr/obelouix/ultimate/config/BaseConfig.java
+++ src/main/java/fr/obelouix/ultimate/config/BaseConfig.java
@@ -4,9 +4,17 @@ import org.spongepowered.configurate.serialize.SerializationException;
 
 public abstract class BaseConfig {
 
+    /**
+     * Used for adding options into the config file
+     *
+     * @throws SerializationException
+     */
     protected void addToConfig() throws SerializationException {
     }
 
+    /**
+     * Read the config file and setup the plugin
+     */
     protected void readConfig() {
     }
 
