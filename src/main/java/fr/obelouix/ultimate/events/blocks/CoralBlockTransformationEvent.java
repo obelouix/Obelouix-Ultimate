1f74750b848328177e52b535831b8ebdc3c97bc9
Louis <3874662+obelouix@users.noreply.github.com>
2022-08-21 17:35:15 +0200

    increased transformed to 9 ~~was 1~~

--- src/main/java/fr/obelouix/ultimate/events/blocks/CoralBlockTransformationEvent.java
+++ src/main/java/fr/obelouix/ultimate/events/blocks/CoralBlockTransformationEvent.java
@@ -2,6 +2,7 @@ package fr.obelouix.ultimate.events.blocks;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
@@ -10,29 +11,48 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionType;
 
+import java.util.stream.Stream;
+
 public class CoralBlockTransformationEvent implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void reviveDeadCoral(ProjectileHitEvent event) {
         final Projectile projectile = event.getEntity();
+        final Block hitBlock = event.getHitBlock();
 
         if (projectile instanceof ThrownPotion potion) {
             final PotionMeta potionMeta = potion.getPotionMeta();
             if (potionMeta.getBasePotionData().getType() == PotionType.REGEN) {
-                if (event.getHitBlock() == null) event.setCancelled(true);
-
-                final Block hitBlock = event.getHitBlock();
-                switch (hitBlock.getType()) {
-                    case DEAD_BRAIN_CORAL_BLOCK -> hitBlock.setType(Material.BRAIN_CORAL_BLOCK);
-                    case DEAD_BUBBLE_CORAL_BLOCK -> hitBlock.setType(Material.BUBBLE_CORAL_BLOCK);
-                    case DEAD_FIRE_CORAL_BLOCK -> hitBlock.setType(Material.FIRE_CORAL_BLOCK);
-                    case DEAD_HORN_CORAL_BLOCK -> hitBlock.setType(Material.HORN_CORAL_BLOCK);
-                    case DEAD_TUBE_CORAL_BLOCK -> hitBlock.setType(Material.TUBE_CORAL_BLOCK);
-                    default -> event.setCancelled(true);
-                }
+                if (hitBlock == null) event.setCancelled(true);
+                else transform(hitBlock);
             }
         } else event.setCancelled(true);
 
+    }
+
+    private void transform(Block block) {
+
+        Stream.of(
+                BlockFace.NORTH_WEST,
+                BlockFace.NORTH,
+                BlockFace.NORTH_EAST,
+                BlockFace.WEST,
+                BlockFace.SELF,
+                BlockFace.EAST,
+                BlockFace.SOUTH_WEST,
+                BlockFace.SOUTH,
+                BlockFace.SOUTH_EAST
+        ).map(block::getRelative).forEach(blockFace -> {
+            switch (blockFace.getType()) {
+                case DEAD_BRAIN_CORAL_BLOCK -> blockFace.setType(Material.BRAIN_CORAL_BLOCK);
+                case DEAD_BUBBLE_CORAL_BLOCK -> blockFace.setType(Material.BUBBLE_CORAL_BLOCK);
+                case DEAD_FIRE_CORAL_BLOCK -> blockFace.setType(Material.FIRE_CORAL_BLOCK);
+                case DEAD_HORN_CORAL_BLOCK -> blockFace.setType(Material.HORN_CORAL_BLOCK);
+                case DEAD_TUBE_CORAL_BLOCK -> blockFace.setType(Material.TUBE_CORAL_BLOCK);
+                default -> {
+                }
+            }
+        });
     }
 
 }
