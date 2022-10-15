12ae4d72b4002df841cd038e167559c53a2c9a12
Louis <3874662+obelouix@users.noreply.github.com>
2022-10-15 22:18:52 +0200

    added more methods

--- src/main/java/fr/obelouix/ultimate/api/Book.java
+++ src/main/java/fr/obelouix/ultimate/api/Book.java
@@ -4,6 +4,10 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  * Create a book as an {@link ItemStack}
@@ -19,12 +23,21 @@ public class Book {
     }
 
     /**
+     * Gets the author of the book.
+     *
+     * @return the author of the book.
+     */
+    public Component getAuthor() {
+        return bookMeta.hasAuthor() ? bookMeta.author() : null;
+    }
+
+    /**
      * Sets the author of the book.
      * See {@link BookMeta#author(Component)}
      *
      * @param author the author to set
      */
-    public Book setAuthor(Component author) {
+    public Book setAuthor(@Nullable Component author) {
         bookMeta = bookMeta.author(author);
         return this;
     }
@@ -33,9 +46,60 @@ public class Book {
      * Adds new pages to the end of the book. Up to a maximum of 50 pages with 256 characters per page
      * See {@link BookMeta#addPages(Component...)}
      */
-    public Book addPages(Component... pages) {
+    public Book addPages(@NotNull Component... pages) {
         bookMeta.addPages(pages);
         return this;
+    }
+
+    /**
+     * Sets the specified page in the book.
+     * See {@link BookMeta#page(int, Component)}
+     *
+     * @param page the page to set
+     * @param data the data to set for that page
+     */
+    public Book setPage(int page, Component data) {
+        bookMeta.page(page, data);
+        bookItem.setItemMeta(bookMeta);
+        return this;
+    }
+
+    /**
+     * Gets the specified page in the book. The page must exist.
+     * Pages are 1-indexed
+     *
+     * @param page the page number to get, in range [1, getPageCount()]
+     * @return the page from the book
+     */
+    public Component getPage(int page) {
+        return bookMeta.page(page);
+    }
+
+    /**
+     * Gets the number of pages in the book.
+     *
+     * @return the number of pages in the book.
+     */
+    public int getPageCount() {
+        return bookMeta.getPageCount();
+    }
+
+    /**
+     * Gets the list of pages.
+     *
+     * @return an unmodifiable list of pages
+     */
+    public List<Component> getPages() {
+        return bookMeta.pages();
+    }
+
+    /**
+     * Gets the title of the book.
+     *
+     * @return the title of the book
+     */
+    public Component getTitle() {
+        return bookMeta.hasTitle() ? bookMeta.title() : null;
     }
 
     /**
@@ -45,7 +109,7 @@ public class Book {
      *
      * @param title the title to set
      */
-    public Book setTitle(Component title) {
+    public Book setTitle(@Nullable Component title) {
         bookMeta = bookMeta.title(title);
         return this;
     }
@@ -58,5 +122,14 @@ public class Book {
     public ItemStack build() {
         bookItem.setItemMeta(bookMeta);
         return bookItem;
+    }
+
+    /**
+     * Get the {@link BookMeta}
+     *
+     * @return the book meta
+     */
+    public BookMeta getBookMeta() {
+        return bookMeta;
     }
 }
