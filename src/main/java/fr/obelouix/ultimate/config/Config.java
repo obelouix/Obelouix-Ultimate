3e1a730f97532a6f70f04463e796fc5bab09c0a7
Louis <3874662+obelouix@users.noreply.github.com>
2023-03-04 14:59:03 +0100

    cleanup

--- src/main/java/fr/obelouix/ultimate/config/Config.java
+++ src/main/java/fr/obelouix/ultimate/config/Config.java
@@ -11,7 +11,10 @@ import org.spongepowered.configurate.serialize.SerializationException;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class Config {
@@ -20,36 +23,20 @@ public class Config {
             .path(Path.of(Main.getPlugin().getDataFolder().getPath(), "config.conf"))
             .build();
 
-    private static CommentedConfigurationNode paperRoot;
 
-    private static boolean debugMode;
-    private static final HashMap<String, Boolean> dynmapStructureMap = new HashMap<>();
-    public static final Map<String, String> chatFormat = new HashMap<>();
-    private static String customServerBrandName;
-    private static boolean disableReloadCommand = false;
-    private static String storageType;
     private static CommentedConfigurationNode root;
+
     private static boolean configReloaded = true;
+    private static boolean disableReloadCommand = false;
     private static boolean disableWitherBlockDamage = false;
     private static boolean showWitherSkullExplosionsParticles = false;
-    private static boolean serverInMaintenance = false;
-    private static boolean dynmapStructuresEnabled = false;
-    private static boolean dynmapWorldGuardEnabled = false;
-    private static String dynmapWorldGuardLayer = "";
     private static boolean isAnvilInfiniteRepairEnabled = false;
     private static boolean isFastLeafDecayEnabled = false;
-    private static boolean unloadEmptyWorlds = false;
-    private static boolean disconnectOnHighPing = false;
-    private static int maxPing;
-    private static boolean nightSkipSystemEnabled;
-    private static boolean EssentialsAFKHook;
     private static final Set<World> coordinatesBlacklist = new HashSet<>();
 
     private static boolean giveEnderDragon = false;
-    private static String databaseUsername;
-    private static String databasePassword;
-    private static String databaseUrl;
-    private static int databasePort;
+
+    private static boolean reviveCoralBlock;
 
     public static void loadConfig() {
         try {
@@ -75,69 +62,13 @@ public class Config {
         }
 
         giveEnderDragon = root.node("tweaks", "give-enderdragon-exp-to-everyone").getBoolean();
-        debugMode = root.node("debug").getBoolean();
-
-        customServerBrandName = root.node("custom-server-brand").getString();
-
-/*        storageType = root.node("data", "storage-type").getString();
-
-        if (storageType.equalsIgnoreCase("mysql")) {
-            databaseUrl = root.node("data", "database", "url").getString();
-            databasePort = root.node("data", "database", "port").getInt();
-
-        }
-        if (List.of("mysql", "h2").stream().anyMatch(s -> storageType.equalsIgnoreCase(s))) {
-            databaseUsername = root.node("data", "database", "username").getString();
-            databasePassword = root.node("data", "database", "password").getString();
-        }*/
-
 
         disableReloadCommand = root.node("disable-default-reload-command").getBoolean();
-        /*if (LuckPermsUtils.getLuckPermsAPI() != null) {
-            for (final Object group : root.node("chat", "format").childrenMap().keySet()) {
-                chatFormat.put(group.toString(), root.node("chat", "format", group.toString()).getString());
-            }
-
-        }*/
-
-        /*if (PluginDetector.getDynmap() != null) {
-
-            dynmapStructuresEnabled = root.node("dynmap", "modules", "structures", "enabled").getBoolean();
-
-            if (dynmapStructuresEnabled) {
-                dynmapStructuresEnabled = false;
-                plugin.getComponentLogger().info("Dynmap Structures is disabled until the lag it cause is resolved");
-            }
-
-            if (PluginDetector.getWorldGuard() != null) {
-                dynmapWorldGuardEnabled = root.node("dynmap", "modules", "worldguard", "enabled").getBoolean();
-                dynmapWorldGuardLayer = root.node("dynmap", "modules", "worldguard", "layer_name").getString();
-            }
-        }*/
-
         isAnvilInfiniteRepairEnabled = root.node("anvil", "infinite-repair").getBoolean();
         disableWitherBlockDamage = root.node("protection", "explosions", "wither", "disable-block-damage").getBoolean();
         showWitherSkullExplosionsParticles = root.node("protection", "explosions", "wither", "show-wither-skull-explosions-particles").getBoolean();
-        serverInMaintenance = root.node("maintenance").getBoolean();
         isFastLeafDecayEnabled = root.node("fast-leaf-decay", "enabled").getBoolean();
         Main.getPlugin().getComponentLogger().info("Configuration loaded");
-
-        unloadEmptyWorlds = root.node("worlds", "unload_if_empty").getBoolean();
-
-        disconnectOnHighPing = root.node("ping", "disconnect-if-too-high").getBoolean();
-
-        if (disconnectOnHighPing) maxPing = root.node("ping", "threshold").getInt();
-        EssentialsAFKHook = root.node("night-skipper", "essentials-hook").getBoolean();
-
-        nightSkipSystemEnabled = root.node("night-skipper", "enabled").getBoolean();
-
-/*        if (EssentialsAFKHook && PluginDetector.getEssentials() != null) {
-            try {
-                root.node("night-skipper", "essentials-hook").set(Boolean.FALSE);
-            } catch (SerializationException e) {
-                e.printStackTrace();
-            }
-        }*/
 
         try {
             Objects.requireNonNull(root.node("coordinates", "world", "blacklist").getList(String.class)).forEach(
@@ -173,12 +104,7 @@ public class Config {
      */
     private static void addMissingConfigs() throws SerializationException {
 
-        CompletableFuture.runAsync(() -> {
-            addStorageSettings();
-            addDynmapSettings();
-            addFeatureSettings();
-            addLuckPermsSettings();
-        });
+        CompletableFuture.runAsync(Config::addFeatureSettings);
 
 
         final CommentedConfigurationNode enderDragonExpShare = root.node("tweaks", "give-enderdragon-exp-to-everyone");
@@ -298,70 +224,6 @@ public class Config {
         );
     }
 
-    private static void addDynmapSettings() {
-/*        CompletableFuture.runAsync(() -> {
-            try {
-                if (PluginDetector.getDynmap() != null) {
-                    if (root.node("dynmap", "modules", "structures", "enabled").empty()) {
-                        root.node("dynmap", "modules", "structures", "enabled").set(Boolean.TRUE);
-                    }
-                    if (root.node("dynmap", "modules", "structures", "layer_name").empty()) {
-                        root.node("dynmap", "modules", "structures", "layer_name").set("Structures");
-//                    .commentIfAbsent("A server restart is required to change the name, a reload will not work");
-                    }
-                    if (PluginDetector.getWorldGuard() != null) {
-                        if (root.node("dynmap", "modules", "worldguard", "enabled").empty()) {
-                            root.node("dynmap", "modules", "worldguard", "enabled").set(Boolean.TRUE);
-                        }
-                        if (root.node("dynmap", "modules", "worldguard", "layer_name").empty()) {
-                            root.node("dynmap", "modules", "worldguard", "layer_name").set("WorldGuard");
-                        }
-                    }
-                }
-            } catch (SerializationException e) {
-                throw new RuntimeException();
-            }
-        });*/
-    }
-
-    private static void addLuckPermsSettings() {
-      /*  CompletableFuture.runAsync(() -> {
-            try {
-                // Get all groups and generate the config dynamically
-                if (LuckPermsUtils.getLuckPermsAPI() != null && root.node("chat").empty()) {
-                    for (final Group group : LuckPermsUtils.getGroups()) {
-                        root.node("chat").act(n -> {
-                            if (group.getName().equals("default")) {
-                                n.node("format")
-                                        .commentIfAbsent("""
-                                                This allow you to control the chat formatting for every groups
-                                                You case use standard color code like &4 and also hex color codes like &#808080
-
-                                                Some tags are available to allow you to fully customize the chat:
-                                                - {displayname} : player name
-                                                - {message} : the message of the player
-                                                - {world} : the current world of the player
-                                                - {prefix} : the luckperms prefix of the group
-                                                - {suffix} : the luckperms suffix of the group
-
-                                                /!\\ PLEASE NOTE THAT IF YOU ADD COLOR CODES DIRECTLY IN LUCKPERMS META FOR
-                                                PREFIX AND SUFFIX, THEY WILL OVERRIDE PREFIX AND SUFFIX COLORS FROM THIS CONFIG
-                                                """)
-                                        .node(group.getName()).set("&#808080{displayname}: {message}")
-                                        .commentIfAbsent("default LuckPerms group");
-                            } else {
-                                n.node("format").node(group.getName()).set("&#32cd32[{world}]&r{prefix}{displayname}{suffix}: &r{message}")
-                                        .commentIfAbsent(group.getName() + " group");
-                            }
-
-                        });
-                    }
-                }
-            } catch (SerializationException e) {
-                throw new RuntimeException(e);
-            }
-        });*/
-    }
 
     private static void addFeatureSettings() {
         CompletableFuture.runAsync(() -> {
@@ -397,13 +259,7 @@ public class Config {
         return giveEnderDragon;
     }
 
-    public static boolean isDebugMode() {
-        return debugMode;
-    }
 
-    public static String getStorageType() {
-        return storageType;
-    }
 
     public static boolean isDisableReloadCommand() {
         return disableReloadCommand;
@@ -411,10 +267,6 @@ public class Config {
 
     public static boolean isConfigReloaded() {
         return configReloaded;
-    }
-
-    public static String getCustomServerBrandName() {
-        return customServerBrandName;
     }
 
     public static boolean isWitherBlockDamageDisabled() {
@@ -425,43 +277,8 @@ public class Config {
         return showWitherSkullExplosionsParticles;
     }
 
-    public static boolean isServerInMaintenance() {
-        return serverInMaintenance;
-    }
-
-    public static void setServerInMaintenance(boolean serverInMaintenance) {
-        Config.serverInMaintenance = serverInMaintenance;
-        try {
-            root.node("maintenance").set(serverInMaintenance);
-            save(Config.getRoot());
-        } catch (SerializationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static boolean isDynmapStructuresEnabled() {
-        return dynmapStructuresEnabled;
-    }
-
-    public static String getDynmapStructuresLayerName() {
-        String dynmapStructuresLayerName = "";
-        return dynmapStructuresLayerName;
-    }
-
-    public static boolean isDynmapWorldGuardEnabled() {
-        return dynmapWorldGuardEnabled;
-    }
-
-    public static HashMap<String, Boolean> getDynmapStructureMap() {
-        return dynmapStructureMap;
-    }
-
     public static CommentedConfigurationNode getRoot() {
         return root;
-    }
-
-    public static String getDynmapWorldGuardLayer() {
-        return dynmapWorldGuardLayer;
     }
 
     public static boolean isFastLeafDecayEnabled() {
@@ -472,47 +289,11 @@ public class Config {
         return isAnvilInfiniteRepairEnabled;
     }
 
-    public static boolean shouldUnloadEmptyWorlds() {
-        return unloadEmptyWorlds;
-    }
-
-    public static boolean isDisconnectOnHighPing() {
-        return disconnectOnHighPing;
-    }
-
-    public static int getMaxPing() {
-        return maxPing;
-    }
-
-    public static void setMaxPing(int maxPing) {
-        Config.maxPing = maxPing;
-    }
-
-    public static boolean isNightSkipSystemEnabled() {
-        return nightSkipSystemEnabled;
-    }
-
-    public static boolean isEssentialsAFKHook() {
-        return EssentialsAFKHook;
-    }
-
     public static Set<World> getCoordinatesBlacklist() {
         return coordinatesBlacklist;
     }
 
-    public static String getDatabaseUsername() {
-        return databaseUsername;
-    }
-
-    public static String getDatabasePassword() {
-        return databasePassword;
-    }
-
-    public static String getDatabaseUrl() {
-        return databaseUrl;
-    }
-
-    public static int getDatabasePort() {
-        return databasePort;
+    public static boolean canReviveCoralBlock() {
+        return reviveCoralBlock;
     }
 }
