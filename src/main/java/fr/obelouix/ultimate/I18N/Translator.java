b1083c4630f2ce7d66fe58475edc92f54766a128
Louis <3874662+obelouix@users.noreply.github.com>
2023-03-05 08:36:05 +0100

    fixed translation bug (text always in english) & moved translations files

--- src/main/java/fr/obelouix/ultimate/I18N/Translator.java
+++ src/main/java/fr/obelouix/ultimate/I18N/Translator.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationRegistry;
 import net.kyori.adventure.util.UTF8ResourceBundleControl;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Locale;
@@ -20,25 +19,27 @@ public class Translator {
     public static void init() {
         registerAllTranslations(
                 List.of(
-                        new Locale("en", "US"),
+                        Locale.US,
+                        Locale.FRANCE,
+                        Locale.CANADA_FRENCH
+       /*                 new Locale("en", "US"),
                         new Locale("fr", "FR"),
-                        new Locale("fr", "CA")
+                        new Locale("fr", "CA")*/
                 )
         );
     }
 
     private static void registerAllTranslations(List<Locale> locales) {
+        final TranslationRegistry registry = TranslationRegistry.create(Key.key("obelouix", "main"));
+
         locales.forEach(
                 locale -> {
                     final String language = locale.toLanguageTag().replace("-", "_");
-                    final TranslationRegistry registry = TranslationRegistry.create(Key.key("obelouix", "main"));
-
-                    registry.defaultLocale(Locale.FRENCH);
 
                     final ResourceBundle resourceBundle = ResourceBundle.getBundle(
-                            "lang_" + language, locale, UTF8ResourceBundleControl.get());
+                            "translations." + language, locale, UTF8ResourceBundleControl.get());
 
-                    registry.registerAll(locale, resourceBundle, true);
+                    registry.registerAll(locale, resourceBundle, false);
 
                     if (globalTranslator.addSource(registry)) {
                         Main.getPlugin().getComponentLogger().info(Component.text("Registered locale: " + language, NamedTextColor.GREEN));
@@ -46,10 +47,16 @@ public class Translator {
                 }
         );
 
+        registry.defaultLocale(Locale.US);
+
     }
 
-    public static Component translate(TranslatableComponent component, @Nullable Locale locale) {
-        return GlobalTranslator.render(component, locale == null ? new Locale("en", "US") : locale);
+    public static Component translate(TranslatableComponent component, Locale locale) {
+        return GlobalTranslator.render(component, locale);
+    }
+
+    public static Component translate(Component component, Locale locale) {
+        return GlobalTranslator.render(component, locale);
     }
 
 }
