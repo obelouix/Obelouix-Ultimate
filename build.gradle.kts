b79ae96162f014f040840271614db6648b49ffbe
Louis <3874662+obelouix@users.noreply.github.com>
2023-04-02 22:18:33 +0200

    added missings parts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,34 @@ subprojects {
     if (!project.name.contains("common")) {
         plugins.apply("io.papermc.paperweight.userdev")
         plugins.apply("xyz.jpenilla.run-paper")
-
     }
 
+    java {
+        // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+
+    tasks {
+        // Configure reobfJar to run when invoking the build task
+        assemble {
+            dependsOn(reobfJar)
+        }
+
+        compileJava {
+            options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+            // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+            // See https://openjdk.java.net/jeps/247 for more information.
+            options.release.set(17)
+        }
+        javadoc {
+            options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+        }
+        processResources {
+            filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        }
+    }
 
 }
 
